1edd24b00f6b931393194d05c8338fe61c7abc9f
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-13 01:01:26 +0900

    feat(Sidebar): toggle hiding

--- src/components/sidebar/Sidebar.svelte
+++ src/components/sidebar/Sidebar.svelte
@@ -1,9 +1,15 @@
-<script>
+<script lang="ts">
     import SideButton from "./SideButton.svelte"
+
+    export let isPinned = true
 </script>
 
 <div class="sb-wrap">
-    <div class="sidebar">
+    <div class="sb" class:sb-is-pinned={isPinned}>
+        <button
+            class="sb-btn"
+            on:click={() => isPinned = !isPinned}
+        />
         <SideButton
             icon="/mc.png"
         />
@@ -14,7 +20,7 @@
 <style lang="scss">
   @import "../../scss/vars/sidebar.scss";
 
-  .sidebar {
+  .sb {
     position: fixed;
     background-color: rgba(245, 245, 245, 0.85);
     width: $sidebar-width;
@@ -27,7 +33,16 @@
     border-radius: 1.2rem;
     margin: auto 0;
 
-    z-index: 999;
+    opacity: 0.65;
+    left: 3.4rem;
+
+    transition-duration: 0.2s;
+
+    &:hover {
+      left: 0;
+      opacity: 1;
+      z-index: 1;
+    }
   }
 
   .sb-wrap {
@@ -36,5 +51,34 @@
     flex-direction: column;
     justify-content: center;
     height: 100%;
+  }
+
+  .sb-is-pinned {
+    left: 0;
+    opacity: 1;
+    z-index: 1;
+  }
+
+  .sb-is-pinned .sb-btn {
+    background-color: rgb(255, 40, 40);
+  }
+
+  .sb-btn {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    background-color: rgb(255, 90, 0);
+    border: none;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    cursor: pointer;
+    transition-duration: 0.12s;
+    z-index: 2;
+
+    &:hover {
+      background-color: rgb(255, 40, 40);
+    }
   }
 </style>
