061b430faaca18ff1dd93e5c725cabd69c89ef2c
pleahmacaka <pleahmacaka>
2023-11-14 10:01:43 +0900

    chore: change default window size, declare zoom global type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,11 @@
     import "../scss/global.scss"
     import Titlebar from "../components/Titlebar.svelte"
     import BasePanel from "../components/panels/BasePanel.svelte"
+    import { onMount } from "svelte"
+
+    onMount(() => {
+        document.body.style.zoom = 0.8
+    })
 </script>
 
 <Titlebar />
---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+    /**
+     * @description This feature is non-standard and it will no work for every user.
+     * @link https://github.com/tauri-apps/tauri/issues/3310
+     *       https://github.com/microsoft/TypeScript/issues/49253
+     *       https://developer.mozilla.org/en-US/docs/Web/CSS/zoom
+     */
+    export interface CSSStyleDeclaration {
+        zoom: number
+    }
+}
