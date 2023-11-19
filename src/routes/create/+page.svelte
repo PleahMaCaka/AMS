8790d309024d3f4aa260e8cb91271be4ebe35eca
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-19 23:11:48 +0900

    chore: simplified the layout and move codebase to create page

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -2,23 +2,49 @@
     import BigTitleButton from "../../components/panels/create/ServerTypeButton.svelte"
 </script>
 
-<p>Choose type of server</p>
-<h2>Vanilla or Plugin</h2>
-<div class="serv-type-selection">
-    <BigTitleButton
-        icon="/logo/mc.png"
-        title="Vanilla"
-        label="None"
-    />
-    <BigTitleButton
-        icon="/logo/paper.png"
-        title="Paper"
-        label="Recommended"
-    />
+<div class="create-page">
+    <h1>Create a new server</h1>
+    <p>Select the type of server you want to create.</p>
+    <hr />
+
+    <div class="serv-type-selection">
+        <BigTitleButton
+            icon="/logo/mc.png"
+            title="Vanilla"
+            label="None"
+        />
+        <BigTitleButton
+            icon="/logo/paper.png"
+            title="Paper"
+            label="Recommended"
+        />
+    </div>
 </div>
 
 <style lang="scss">
     @import "../../scss/anim/appear.scss";
+
+    .create-page {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100%;
+    }
+
+    h1 {
+        margin-bottom: 0;
+    }
+
+    hr {
+        opacity: 0.7;
+        @include expand-width(40%);
+    }
 
     .serv-type-selection {
         display: flex;
@@ -26,3 +52,4 @@
         justify-content: center;
     }
 </style>
+
