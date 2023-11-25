6dbb54f2bb0fb71e9263d0e351afb91f7da3dd7a
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-26 01:42:19 +0900

    chore(ToggleOption): disable toggle by prop

--- src/components/settings/ToggleOption.svelte
+++ src/components/settings/ToggleOption.svelte
@@ -1,42 +1,51 @@
 <script lang="ts">
 
     import type { Writable } from "svelte/store"
-    import { get } from "svelte/store"
 
     export let title: string = "Toggle Option!"
     export let description: string = "Do you know about that? A toggle option is a checkbox that can be toggled on or off."
 
     export let store: Writable<boolean> | undefined = undefined
+    export let disable: boolean = false
 
     function toggleCheckbox() {
         if (store === undefined)
             return console.log(`The Store is not connected in the "${title}" toggle option.`)
 
         store.update((value) => !value)
-        console.log(get(store))
     }
 </script>
 
 <div class="toggle-option-container">
     <section>
         <h3>{title}</h3>
-        <button class="toggle-ckbx" on:click={toggleCheckbox}>
-            <input type="checkbox" bind:checked={$store} />
-            <span></span>
+        <button class:is-disabled={disable}
+                on:click={toggleCheckbox}
+                disabled={disable}
+        >
+            <input type="checkbox" bind:checked={$store} disabled={disable} />
+            <span />
         </button>
     </section>
     <p>{description}</p>
 </div>
 
 <style lang="scss">
+    .is-disabled {
+        opacity: 0.5 !important;
+        box-shadow: 0 0 0 0.2rem rgba(255, 0, 0, 0.5) !important;
+        cursor: not-allowed !important;
+    }
+
     .toggle-option-container {
         display: flex;
         flex-direction: column;
         width: 100%;
         height: 100%;
+        margin: 0.6rem 0 0 0;
 
         p {
-            margin: 2px 0 0 0;
+            margin: 0.2rem 0 0 0;
         }
 
         section {
@@ -96,6 +105,8 @@
 
                 input:checked + span::after {
                     transform: translateX(10px);
+                    transition-duration: 0.13s;
+                    background-color: #656565;
                 }
             }
         }
