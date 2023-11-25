92c3c7bc8137a9f63712a632d6ae784d8fb3ad66
PleahMaCaka <pleahmacaka@gmail.com>
2023-11-26 01:00:07 +0900

    feat(`/settings`): use default jvm option

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,23 +1,44 @@
+<script lang="ts">
+    import ToggleOption from "../../components/settings/ToggleOption.svelte"
+    import { configStore } from "../../stores/configStore"
+</script>
+
 <div class="settings-page">
     <h1>Settings</h1>
     <hr />
+    <div class="option-container">
+        <ToggleOption
+            title="Use Default JAVA_HOME"
+            description="Use the default JAVA_HOME environment variable instead of the one specified in the settings."
+            store={configStore.useDefaultJavaHome}
+        />
+    </div>
 </div>
 
 <style lang="scss">
     @import "../../scss/anim/appear.scss";
 
     .settings-page {
+        display: block;
+        box-sizing: border-box;
         width: 100%;
         height: 100%;
 
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-
         animation: fade-in-up 0.2s ease-in-out;
 
-        hr {
-            @include expand-width(40%, 0.73s)
+        h1 {
+            font-size: 2.5rem;
+            font-weight: 500;
+            text-align: center;
         }
+
+        hr {
+            @include expand-width(40%, 0.73s);
+        }
+    }
+
+    .option-container {
+        height: auto;
+        margin: 0 10%;
     }
 </style>
---
+++ src/stores/configStore.ts
@@ -0,0 +1,5 @@
+import { writable } from "svelte/store"
+
+export const configStore = {
+    useDefaultJavaHome: writable<boolean>(true)
+}
